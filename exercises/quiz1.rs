70bb4788e22fd25259294b4e4afda7691410c1c8
rvyou <rvyou@qq.com>
2024-03-29 17:58:00 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,7 +13,7 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+// I AM  DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
@@ -30,4 +30,10 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+fn calculate_price_of_apples(da:i32)->i32{
+    if da >40 {
+        return da;
+    }
+    da*2
 }
